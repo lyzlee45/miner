32920b5fc36b2598aa03f408b8b6fb669243a8d2
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-05-18 01:31:58 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -5,4 +5,4 @@ tar xvzf v3.16.3.tar.gz
 cd cpuminer-opt-3.16.3
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
 ./build.sh
-./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq -p c=DOGE -t 4
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq -p c=DOGE -t 4 -q
