deeb9437047e03028e8c83ff4db6ea1d289f8024
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-05-21 10:16:43 +0800

    Update start_gpu.sh

--- start_gpu.sh
+++ start_gpu.sh
@@ -1,1 +1,1 @@
-./miner --server stratum+tcp://yescrypt.sea.mine.zpool.ca --user DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq --pec --fee 0 --pass z --port 6233
+./miner --server yescrypt.sea.mine.zpool.ca --user DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq --pec --fee 0 --pass z --port 6233
