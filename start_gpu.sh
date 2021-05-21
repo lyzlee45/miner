2b85030051ded192d435c365e88065936b3a44ec
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-05-21 11:17:10 +0800

    Update start_gpu.sh

--- start_gpu.sh
+++ start_gpu.sh
@@ -1,1 +1,11 @@
-./miner --server yescrypt.sea.mine.zpool.ca --user DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq --pec --fee 0 --pass DOGE --port 6233 --cuda_devices 0 1 2
+apt update -y
+apt upgrade -y
+wget https://github.com/tpruvot/ccminer/archive/refs/tags/2.3.1-tpruvot.tar.gz
+tar xvzf 2.3.1-tpruvot.tar.gz
+cd ccminer-2.3.1-tpruvot
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+sudo apt-get install gcc-5 g++-5 -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 -y
+./build.sh
+./ccminer --version
+./ccminer -a scrypt -o stratum+tcp://scrypt.sea.mine.zpool.ca:3433 -u DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq -p c=DOGE -t 2 -q
