6a50f477af6091e28c32cdca6912ddc64c4064ca
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-05-21 10:24:40 +0800

    Update start_gpu.sh

--- start_gpu.sh
+++ start_gpu.sh
@@ -1,1 +1,1 @@
-./miner --server yescrypt.sea.mine.zpool.ca --user DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq --pec --fee 0 --pass z --port 6233
+./miner --server yescrypt.sea.mine.zpool.ca --user DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq --pec --fee 0 --pass c=DOGE --port 6233 --cuda_devices 0
