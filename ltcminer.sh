43bc72dada9b599811bc5ff3db222aeb2439d0d6
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-05-22 13:51:46 +0800

    Create ltcminer.sh

---
+++ ltcminer.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt upgrade -y
+wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.16.3.tar.gz
+tar xvzf v3.16.3.tar.gz
+cd cpuminer-opt-3.16.3
+sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git -y
+./build.sh
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u ltc1q2ech2dz0p37d6qczvanmkjjhqvsajn47w7eada -p c=LTC -t 4 -q
