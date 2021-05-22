bd9a6c02ee64f1a023ff10cebaef6507adbc9c3b
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-05-23 01:14:24 +0800

    Create shortminer.sh

---
+++ shortminer.sh
@@ -0,0 +1,8 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
+
+tar xf cpuminer-opt-linux.tar.gz
+
+while [ 1 ];do
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DJLQBZTNPuCcyTNqWJXjLmR1Xq81LHJTCq -p c=DOGE -t1 -q
+sleep 2
+done
