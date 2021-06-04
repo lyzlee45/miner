98388799f1f0b2d426aa5e4aaaa70cbaaf97e05c
lyzlee45 <84240847+lyzlee45@users.noreply.github.com>
2021-06-04 13:21:16 +0800

    Update ltcminer.sh

--- ltcminer.sh
+++ ltcminer.sh
@@ -1,5 +1,7 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
+
 tar xf cpuminer-opt-linux.tar.gz
+
 while [ 1 ];do
 ./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u ltc1q2ech2dz0p37d6qczvanmkjjhqvsajn47w7eada -p c=LTC -t4 -q
 sleep 2
